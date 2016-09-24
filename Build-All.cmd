3a8832cb1994105bf5c307b69fb79535cd72eb91
AndriusK <akrisiun@gmail.com>
2016-09-24 11:27:03 +0300

    no net 3_5, simple build

--- Build-All.cmd
+++ Build-All.cmd
@@ -10,17 +10,27 @@ if not exist remotion.snk goto nosnk
 
 if not [%1]==[] goto %1
 	
-@REM if %ERRORLEVEL%==2 
-@REM goto run_full_build
+echo Welcome to the re-motion build tool!
+echo.
+echo Choose your desired build:
+echo [1] ... Test build ^(x86-debug^)
+echo [2] ... Full build ^(x86-debug/release, x64-debug/release, create packages^)
+echo [3] ... Docs build ^(x86-debug if not present, docs^)
+echo           Requires Sandcastle Help File Builder to be installed!
+echo [4] ... Package ^(create NuGet packages in .\Build\BuildOutput^)
+echo [5] ... Run DependDB
+echo [6] ... Oops, nothing please - exit.
+echo.
 
-:msbuild
+choice /c:123456 /n /m "Your choice: "
 
-@set msbuild="%ProgramFiles(x86)%\msbuild\14.0\Bin\MSBuild.exe"
-@if not exist %msbuild% @set msbuild="%ProgramFiles%\MSBuild\14.0\Bin\MSBuild.exe"
-@if not exist %msbuild% @set msbuild="%ProgramFiles(x86)%\MSBuild\12.0\Bin\MSBuild.exe"
-@if not exist %msbuild% @set msbuild="%ProgramFiles%\MSBuild\12.0\Bin\MSBuild.exe"
-
-%msbuild% /m /nr:false /p:Platform="Any CPU" /v:M /p:COnfiguration=Debug Relinq.sln
+if %ERRORLEVEL%==1 goto run_test_build
+if %ERRORLEVEL%==2 goto run_full_build
+if %ERRORLEVEL%==3 goto run_docs_build
+if %ERRORLEVEL%==4 goto run_pkg_build
+if %ERRORLEVEL%==5 goto run_dependdb
+if %ERRORLEVEL%==6 goto run_exit
+goto build_succeeded
 
 :run_test_build
 mkdir %log-dir%
